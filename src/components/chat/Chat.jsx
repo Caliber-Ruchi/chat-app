129036196cf89323d27adb887addfcd50e594623
ruchi <ruchikhandelwal914@gmail.com>
2024-05-27 17:01:00 +0530

    Login UI Done

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,106 @@
+import { useEffect, useRef, useState } from "react"
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react"
 
 const Chat = () => {
+
+const[open,setOpen] = useState(false);
+const[text,setText]  = useState("");
+const endRef = useRef(null)
+
+useEffect(()=>{
+    endRef.current?.scrollIntoView({behavior: "smooth"});
+},[]);
+
+const handleEmoji = e => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false)
+}
+console.log(text)
     return(
-        <div className="chat">Chat</div>
+        <div className="chat">
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <span>Jane Doe</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt="" />
+                    <img src="./video.png" alt="" />
+                    <img src="./info.png " alt="" />
+                </div>
+            </div>
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                            Dicta similique odio amet rem iusto enim laboriosam quis exercitationem 
+                            deserunt repellendus! Illo, deleniti adipisci obcaecati placeat accusantium eum ullam, temporibus nostrum beatae perferendis reprehenderit saepe optio corrupti alias, eius distinctio ipsa. Facere vel officiis deserunt odio optio voluptatibus, nam aliquam repudiandae.
+                        </p>
+                        <span>1 min ago.</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                            Dicta similique odio amet rem iusto enim laboriosam quis exercitationem 
+                            deserunt repellendus! Illo, deleniti adipisci obcaecati placeat accusantium eum ullam, temporibus nostrum beatae perferendis reprehenderit saepe optio corrupti alias, eius distinctio ipsa. Facere vel officiis deserunt odio optio voluptatibus, nam aliquam repudiandae.
+                        </p>
+                        <span>1 min ago.</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                            Dicta similique odio amet rem iusto enim laboriosam quis exercitationem 
+                            deserunt repellendus! Illo, deleniti adipisci obcaecati placeat accusantium eum ullam, temporibus nostrum beatae perferendis reprehenderit saepe optio corrupti alias, eius distinctio ipsa. Facere vel officiis deserunt odio optio voluptatibus, nam aliquam repudiandae.
+                        </p>
+                        <span>1 min ago.</span>
+                    </div>
+                </div>
+                <div className="message own" >
+                    <div className="texts">
+                        <img src="./message_own.jpg" alt="" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                            Dicta similique odio amet rem iusto enim laboriosam quis exercitationem 
+                            deserunt repellendus! Illo, deleniti adipisci obcaecati placeat accusantium eum ullam, temporibus nostrum beatae perferendis reprehenderit saepe optio corrupti alias, eius distinctio ipsa. Facere vel officiis deserunt odio optio voluptatibus, nam aliquam repudiandae.
+                        </p>
+                        <span>1 min ago.</span>
+                    </div>
+                </div>
+                <div ref={endRef}></div>
+            </div>
+            <div className="bottom">
+                <div className="icons">
+                    <img src="./img.png" alt="" />
+                    <img src="./camera.png" alt="" />
+                    <img src="./mic.png" alt="" />
+                </div>
+                <input type="text" 
+                placeholder="Type a message..."  
+                onChange={e=>setText(e.target.value)}
+                value={text}/>
+                <div className="emoji">
+                    <img src="./emoji.png" 
+                    alt="" 
+                    onClick={() => setOpen((prev) =>!prev)}
+                    />
+                    <div className="picker">
+                    <EmojiPicker open={open} onEmojiClick={handleEmoji}/> 
+                    </div>
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
+        </div>
     )
 }
 
